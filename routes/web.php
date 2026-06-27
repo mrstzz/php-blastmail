513462b30b795cfa947734bac2420c4b6aed78b0
Matheus Montovaneli <matheusmontovaneli2003@gmail.com>
2026-06-27 13:42:16 -0300

    feat: add subscriber controller and view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\EmailListController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubscriberController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -23,10 +24,14 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // Email List
-    Route::get('email-list', [EmailListController::class, 'index'])->name('email-list.index');
-    Route::get('email-list/create', [EmailListController::class, 'create'])->name('email-list.create');
-    
-    Route::post('email-list/store', [EmailListController::class, 'store'])->name('email-list.store');
+    Route::get('/email-list', [EmailListController::class, 'index'])->name('email-list.index');
+    Route::get('/email-list/create', [EmailListController::class, 'create'])->name('email-list.create');
+    Route::post('/email-list/store', [EmailListController::class, 'store'])->name('email-list.store');
+
+    // Subscriber
+    Route::get('/email-list/{emailList}/subscribers', [SubscriberController::class, 'index'])->name('subscribers.index');
+    Route::get('/email-list/{emailList}/subscribers/create', fn()=> '' )->name('subscribers.create');
+
 
 });
 
