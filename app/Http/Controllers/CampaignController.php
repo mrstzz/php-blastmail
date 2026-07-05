df9747e004ba7d04d8e722a7d80316d8ac60d209
Matheus Montovaneli <matheusmontovaneli2003@gmail.com>
2026-07-05 20:49:04 -0300

    feat: save info in the session

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -28,39 +28,91 @@ class CampaignController extends Controller
     function create(?string $tab = null) 
     {
         // dd(session('campaigns::create'));
+
+        // session()->forget('campaigns::create');
+
         return view('campaigns.create',[
             'tab' => $tab,
             'form' => match($tab){
                 'template' => '_template',
                 'schedule' => '_schedule',
                 default => '_config'
-            }
+            },
+            'data' => session()->get('campaigns::create',[
+                'name' => null,
+                'subject' => null,
+                'email_list_id' => null,
+                'template_id' => null,
+                'body' => null,
+                'track_click' => null,
+                'track_open' => null,
+                'send_at' => null
+            ]),
+
         ]);
     }
 
     function store(?string $tab = null)
     {
 
+        $toRoute = '';
+        $map = array_merge([
+            'name' => null,
+            'subject' => null,
+            'email_list_id' => null,
+            'template_id' => null,
+            'body' => null,
+            'track_click' => null,
+            'track_open' => null,
+            'send_at' => null
+        ], request()->all());
+
+
 
         if(blank($tab)){
             // ta vindo do index, então redireciona para o /create = aba n°1
 
-            $data = request()->validate([
-
+            request()->validate([
                 'name' => ['required','string','max:255'],
                 'subject' => ['required','string','max:40'],
                 'email_list_id' => ['nullable'],
                 'template_id' => ['nullable'],
             ]);
 
+            $toRoute = route('campaigns.create',['tab'=> 'template']);
+        }
 
-            session()->put('campaigns::create',$data);
+        if($tab === 'template'){
+            request()->validate([
+                'body' => ['required','string'],
+            ]);
 
-            return to_route('campaigns.create',['tab'=> 'template']);
+            $toRoute = route('campaigns.create',['tab'=> 'schedule']);
+        }
+
+        $session = session()->get('campaigns::create',[]);
+        foreach($session as $key => $value) {
+            $newValue = data_get($map, $key);
+
+            if (filled($newValue)) {
+                $session[$key] = $newValue;
+            }
+        }
+
+
+        if($tab === 'schedule'){
+            request()->validate([
+                'send_at' => ['required','date'],
+            ]);
+
+            $toRoute = route('campaigns.index');
 
         }
 
-        
+        session()->put('campaigns::create',$session);
+
+        return response()->redirectTo($toRoute);
+
     }
 
     function destroy(Campaign $campaign)
--- resources/views/campaigns/create/_template.blade.php
+++ resources/views/campaigns/create/_template.blade.php
@@ -1,9 +1,11 @@
 {{-- template --}}
 
 
+@dump($data['body'])
+
 <div>
    <div>
-        <x-input.richtext name="body" :value="old('body')"/>
+        <x-input.richtext name="body" :value="old('body', $data['body'])"/>
         <x-input-error :messages="$errors->get('body')" class="mt-2" />
     </div>
 </div>
