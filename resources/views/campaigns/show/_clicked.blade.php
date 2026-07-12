9e47e736fb0d6d0e2581abb9d797f76867b41247
Matheus Montovaneli <matheusmontovaneli2003@gmail.com>
2026-07-11 23:22:16 -0300

    feat: add new layout

--- resources/views/campaigns/show/_clicked.blade.php
+++ resources/views/campaigns/show/_clicked.blade.php
@@ -1,4 +1,28 @@
+<x-slot name="header">
+    <x-h2> {{ __('Campaigns') }} </x-h2>
 
-<div>
-    <x-alert success no-icon :title="__('Sua campanha está pronta para ser enviada')"/>
+</x-slot>
+
+<div class="space-y-4">
+
+
+    <x-form class="flex justify-end" action="{{ route('campaigns.show', ['what' => $what, 'campaign' => $campaign->id]) }}" get>
+        <x-input.text id="search" :placeholder="__('Search')" type="text" name="search" :value="$search" autofocus />
+    </x-form>
+
+    <x-table :headers="[__('Name'),__('# Clicks'),__('Email')]">
+
+        <x-slot name="body">
+            
+            <tr>
+                <x-table.td> Jeremias </x-table.td>
+                <x-table.td> 1 </x-table.td>
+                <x-table.td> email@.com </x-table.td>
+
+            </tr>
+            
+
+        </x-slot>
+
+    </x-table>
 </div>
