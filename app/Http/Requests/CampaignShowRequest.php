57e31de5bda08d27f52697129b4f8f946fa41c4a
Matheus Montovaneli <matheusmontovaneli2003@gmail.com>
2026-07-11 23:39:37 -0300

    feat: move access logic to the request

---
+++ app/Http/Requests/CampaignShowRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CampaignShowRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+    
+        $campaign = $this->route('campaign');
+        $what = $this->route('what');
+
+        if (is_null($what)) {
+            return to_route('campaigns.show', ['campaign' => $campaign, 'what' => 'statistics']);
+        }
+
+        abort_unless(in_array($what, ['statistics', 'open', 'clicked']), 404);
+
+
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
