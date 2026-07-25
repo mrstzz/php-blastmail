064403567a4b9c300afd9667c19daf59c231efee
Matheus Montovaneli <matheusmontovaneli2003@gmail.com>
2026-07-25 01:16:40 -0300

    test: add setupCreate test

---
+++ tests/Feature/Campaign/Create/SetupTest.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+
+
+test('when saving we need to update campaigns::create session to have all the data', function () {})->todo();
+test('make sure that when we save the form we will be redirect back to the template tab', function () {})->todo();
+test('it should have on the view a list of email lists', function () {})->todo();
+test('it should have on the view a list of templates', function () {})->todo();
+test('it should have on the view a blank tab variable', function () {})->todo();
+test('it should have on the view the form variable set to _config', function () {})->todo();
+test('it should have on the view all the data in the variable data', function () {})->todo();
+
+// 
+
+describe('validation', function () {
+    test('required fields', function () {})->todo();
+    test('name should have a max of 255 characters', function () {})->todo();
+    test('subject should have a max of 40 characters', function () {})->todo();
+    test('valid email lists', function () {})->todo();
+    test('valid templates', function () {})->todo();
+});
