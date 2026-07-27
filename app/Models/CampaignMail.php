6bf246ec6662d9b9b98d7d17c5596012836a582e
Matheus Montovaneli <matheusmontovaneli2003@gmail.com>
2026-07-27 01:14:21 -0300

    test: add show tests

--- app/Models/CampaignMail.php
+++ app/Models/CampaignMail.php
@@ -17,7 +17,7 @@ class CampaignMail extends Model
         return $query->selectRaw('
                 sum(openings) as total_openings
                 , count(subscriber_id) as total_subscribers
-                , count(case when openings > 0 then subscriber_id end) as unique_opens
+                , count(case when openings > 0 then subscriber_id end) as unique_openings
                 , round((cast(count(case when openings > 0 then subscriber_id end) as float) / cast(count(subscriber_id) as float)) * 100) as openings_rate
                 , sum(clicks) as total_clicks
                 , count(case when clicks > 0 then subscriber_id end) as unique_clicks
--- resources/views/campaigns/show/_statistics.blade.php
+++ resources/views/campaigns/show/_statistics.blade.php
@@ -2,9 +2,9 @@
     <x-alert noIcon success :title="__('Your campaign was sent to ' . $query['total_subscribers'] . ' subscribers.')" />
 
     <div class="grid grid-cols-3 gap-5">
-        <x-dashboard.card heading="{{ $query['total_openings'] }}" subheading="{{ __('Opens') }}" />
-        <x-dashboard.card heading="{{ $query['unique_opens'] }}" subheading="{{ __('Unique Opens') }}" />
-        <x-dashboard.card heading="{{ $query['openings_rate'] }}%" subheading="{{ __('Open Rate') }}" />
+        <x-dashboard.card heading="{{ $query['total_openings'] }}" subheading="{{ __('Openings') }}" />
+        <x-dashboard.card heading="{{ $query['unique_openings'] }}" subheading="{{ __('Unique Openings') }}" />
+        <x-dashboard.card heading="{{ $query['openings_rate'] }}%" subheading="{{ __('Openings Rate') }}" />
         <x-dashboard.card heading="{{ $query['total_clicks'] }}" subheading="{{ __('Clicks') }}" />
         <x-dashboard.card heading="{{ $query['unique_clicks'] }}" subheading="{{ __('Unique Clicks') }}" />
         <x-dashboard.card heading="{{ $query['clicks_rate'] }}%" subheading="{{ __('Clicks Rate') }}" />
